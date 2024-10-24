5461c3c891100d274c0d075e99ed1099e389ee11
Jozka Siregar <jozkasiregar@gmail.com>
2024-10-24 09:02:38 +0700

    another one

--- Defend BatFwee TD/Assets/Others/GameManager.cs
+++ Defend BatFwee TD/Assets/Others/GameManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private int selectedTurret = 0;
+
+    public int SelectedTurret { get => selectedTurret; }
+
+    public void SetTurret(int val) { selectedTurret = val; }
+
     public void GameOver()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
--- Defend BatFwee TD/Assets/Tiles/Waypoint.cs
+++ Defend BatFwee TD/Assets/Tiles/Waypoint.cs
@@ -7,10 +7,25 @@ public class Waypoint : MonoBehaviour
     [SerializeField] private Turret[] turretPrefab;
     [SerializeField] private bool isPlaceable;
     public bool IsPlaceable { get => isPlaceable; }
-    [SerializeField] private int turretNum = 0;
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
 
     private void OnMouseDown()
     {
+        int turretNum;
+        if (gameManager != null)
+        {
+            turretNum = gameManager.SelectedTurret;
+        }
+        else
+        {
+            turretNum = 0;
+        }
+
         if (isPlaceable)
         {
             bool isPlaced = turretPrefab[turretNum].CreateTurret(turretPrefab[turretNum], transform.position);
